2588793dffa446056aba4e7a4a46aa8d319d24d0
Iryna Sokolovska <sokirina777@gmail.com>
2023-08-30 23:22:04 +0300

    third

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,13 +1,21 @@
-
+import styled from '@emotion/styled';
 import React from 'react';
 import { TextField } from '@mui/material';
 
+const FilterForm = styled.div`
+margin: 0 auto;
+display: flex;
+flex-direction: column;
+gap: 10px;
+width: 400px;
+`;
+
 export const Filter = ({handleChangeFilter}) => {
     return (
-        <div>
+        <FilterForm>
             <TextField id="filter" label="Find contacts by name" variant="standard" onChange={handleChangeFilter} />
             
-        </div>
+        </FilterForm>
     )
 };
 export default Filter;
