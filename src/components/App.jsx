a2d62711f935f77db90762aa1d5b8c810c1617c7
Iryna Sokolovska <sokirina777@gmail.com>
2023-08-31 14:35:50 +0300

    fifth comit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,14 +7,29 @@ import { Filter } from "./Filter/Filter";
 
 export class App extends Component  {
   state = {
-  contacts: [],
+    contacts: [
+    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+  ],
   filter: ''
   }
   
   addContact = (newContact) => {
+    const normalizedNewContact = newContact;
+    if (
+      this.state.contacts.find(
+        contact => newContact.name.toLowerCase() === normalizedNewContact
+      )
+    ) {
+      return alert(`${newContact.name} is already in contacts!`);
+    }
+      
     this.setState(prevState => ({
       contacts: [...prevState.contacts, newContact]
     }))
+
   };
 
   handleDeleteContact = (id) => {
@@ -43,7 +58,7 @@ export class App extends Component  {
   };
 };
 
-
+export default App;
 
 // export const App = () => {
 //   return (
